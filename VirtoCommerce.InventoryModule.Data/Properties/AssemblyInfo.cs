cd5c001112a6a40721e8c6a883333786815ac9f5
artem-dudarev <ad@virtoway.com>
2016-05-16 15:48:02 +0200

    Fixed assembly title

--- VirtoCommerce.InventoryModule.Data/Properties/AssemblyInfo.cs
+++ VirtoCommerce.InventoryModule.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtoCommerce.PricingModule.Data")]
+[assembly: AssemblyTitle("VirtoCommerce.InventoryModule.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtoCommerce.PricingModule.Data")]
+[assembly: AssemblyProduct("VirtoCommerce.InventoryModule.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
