1d01e54a04a9bb443ec06168a7cabe3fc133073e
artem-dudarev <ad@virtoway.com>
2016-05-16 15:42:28 +0200

    Moved VirtoCommerce.Inventory module to the root directory

--- VirtoCommerce.InventoryModule.Web/Controllers/Api/InventoryModuleController.cs
+++ VirtoCommerce.InventoryModule.Web/Controllers/Api/InventoryModuleController.cs
@@ -1,29 +1,30 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Http.ModelBinding;
+using VirtoCommerce.Domain.Commerce.Services;
 using VirtoCommerce.Domain.Inventory.Services;
-using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.InventoryModule.Web.Binders;
+using VirtoCommerce.InventoryModule.Web.Converters;
+using VirtoCommerce.InventoryModule.Web.Security;
+using VirtoCommerce.Platform.Core.Web.Security;
 using coreModel = VirtoCommerce.Domain.Inventory.Model;
 using webModel = VirtoCommerce.InventoryModule.Web.Model;
-using VirtoCommerce.InventoryModule.Web.Converters;
-using VirtoCommerce.Domain.Commerce.Services;
-using System.Collections.Generic;
-using VirtoCommerce.InventoryModule.Web.Security;
-using VirtoCommerce.InventoryModule.Web.Binders;
-using System.Web.Http.ModelBinding;
 
 namespace VirtoCommerce.InventoryModule.Web.Controllers.Api
 {
-	[RoutePrefix("")]
-	public class InventoryModuleController : ApiController
-	{
-		private readonly IInventoryService _inventoryService;
-		private readonly ICommerceService _commerceService;
-		public InventoryModuleController(IInventoryService inventoryService, ICommerceService commerceService)
-		{
-			_inventoryService = inventoryService;
-			_commerceService = commerceService;
-		}
+    [RoutePrefix("")]
+    public class InventoryModuleController : ApiController
+    {
+        private readonly IInventoryService _inventoryService;
+        private readonly ICommerceService _commerceService;
+
+        public InventoryModuleController(IInventoryService inventoryService, ICommerceService commerceService)
+        {
+            _inventoryService = inventoryService;
+            _commerceService = commerceService;
+        }
 
         /// <summary>
         /// Get inventories of products
@@ -31,29 +32,29 @@ namespace VirtoCommerce.InventoryModule.Web.Controllers.Api
         /// <remarks>Get inventory of products for each fulfillment center.</remarks>
         /// <param name="ids">Products ids</param>
 		[HttpGet]
-		[ResponseType(typeof(webModel.InventoryInfo[]))]
-		[Route("~/api/inventory/products")]
-		public IHttpActionResult GetProductsInventories([ModelBinder(typeof(IdsStringArrayBinder))] string[] ids)
-		{
-			var result = new List<webModel.InventoryInfo>();
-			var allFulfillments = _commerceService.GetAllFulfillmentCenters().ToArray();
-			var inventories = _inventoryService.GetProductsInventoryInfos(ids).ToList();
+        [Route("~/api/inventory/products")]
+        [ResponseType(typeof(webModel.InventoryInfo[]))]
+        public IHttpActionResult GetProductsInventories([ModelBinder(typeof(IdsStringArrayBinder))] string[] ids)
+        {
+            var result = new List<webModel.InventoryInfo>();
+            var allFulfillments = _commerceService.GetAllFulfillmentCenters().ToArray();
+            var inventories = _inventoryService.GetProductsInventoryInfos(ids).ToList();
 
-			foreach (var productId in ids)
-			{
-				foreach (var fulfillment in allFulfillments)
-				{
-					var productInventory = inventories.FirstOrDefault(x => x.ProductId == productId && x.FulfillmentCenterId == fulfillment.Id)
-					    ?? new coreModel.InventoryInfo { FulfillmentCenterId = fulfillment.Id, ProductId = productId };
-				    
+            foreach (var productId in ids)
+            {
+                foreach (var fulfillment in allFulfillments)
+                {
+                    var productInventory = inventories.FirstOrDefault(x => x.ProductId == productId && x.FulfillmentCenterId == fulfillment.Id)
+                        ?? new coreModel.InventoryInfo { FulfillmentCenterId = fulfillment.Id, ProductId = productId };
+
                     var webModelInventory = productInventory.ToWebModel();
-					webModelInventory.FulfillmentCenter = fulfillment.ToWebModel();
-					result.Add(webModelInventory);
-				}
-			}
+                    webModelInventory.FulfillmentCenter = fulfillment.ToWebModel();
+                    result.Add(webModelInventory);
+                }
+            }
 
-			return Ok(result.ToArray());
-		}
+            return Ok(result.ToArray());
+        }
 
         /// <summary>
         /// Get inventories of product
@@ -61,12 +62,12 @@ namespace VirtoCommerce.InventoryModule.Web.Controllers.Api
         /// <remarks>Get inventories of product for each fulfillment center.</remarks>
         /// <param name="productId">Product id</param>
         [HttpGet]
-		[ResponseType(typeof(webModel.InventoryInfo[]))]
-		[Route("~/api/inventory/products/{productId}")]
-		public IHttpActionResult GetProductInventories(string productId)
-		{
-			return GetProductsInventories(new [] { productId });
-		}
+        [Route("~/api/inventory/products/{productId}")]
+        [ResponseType(typeof(webModel.InventoryInfo[]))]
+        public IHttpActionResult GetProductInventories(string productId)
+        {
+            return GetProductsInventories(new[] { productId });
+        }
 
         /// <summary>
         /// Upsert inventory
@@ -74,16 +75,14 @@ namespace VirtoCommerce.InventoryModule.Web.Controllers.Api
         /// <remarks>Upsert (add or update) given inventory of product.</remarks>
         /// <param name="inventory">Inventory to upsert</param>
 		[HttpPut]
-		[ResponseType(typeof(webModel.InventoryInfo))]
-		[Route("~/api/inventory/products/{productId}")]
+        [Route("~/api/inventory/products/{productId}")]
+        [ResponseType(typeof(webModel.InventoryInfo))]
         [CheckPermission(Permission = InventoryPredefinedPermissions.Update)]
-		public IHttpActionResult UpsertProductInventory(webModel.InventoryInfo inventory)
-		{
-			var result = _inventoryService.UpsertInventory( inventory.ToCoreModel() );
+        public IHttpActionResult UpsertProductInventory(webModel.InventoryInfo inventory)
+        {
+            var result = _inventoryService.UpsertInventory(inventory.ToCoreModel());
 
-			return Ok(result);
-		}
-
-
-	}
+            return Ok(result);
+        }
+    }
 }
