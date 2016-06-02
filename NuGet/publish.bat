e35bd03b6fe7b2236ecbf362b08110cfac247e56
artem-dudarev <ad@virtoway.com>
2016-06-02 16:13:32 +0200

    Added VirtoCommerce.InventoryModule.Client

--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,2 +1,4 @@
-set V=2.10.1
+set V=2.10.4
+nuget push VirtoCommerce.InventoryModule.Client.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.InventoryModule.Data.%V%.nupkg -Source nuget.org -ApiKey %1
+pause
