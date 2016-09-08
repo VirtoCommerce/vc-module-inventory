75f30cc7ce077be3695272fe9c3fc469a1546d30
eMazeika <em@virtoway.com>
2016-09-08 15:45:25 +0300

    Client project deleted

--- VirtoCommerce.InventoryModule.Client/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.InventoryModule.Client")]
-[assembly: AssemblyDescription("VirtoCommerce.Inventory API client generated from a Swagger doc")]
