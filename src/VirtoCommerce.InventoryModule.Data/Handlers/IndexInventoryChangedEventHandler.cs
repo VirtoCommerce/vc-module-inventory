af11bafe303588de30602fc729245b8e866c6209
Andrei Kostyrin <kostyrin2@gmail.com>
2019-10-22 12:20:12 +0200

    #vp-732 IndexingJobs

--- src/VirtoCommerce.InventoryModule.Data/Handlers/IndexInventoryChangedEventHandler.cs
+++ src/VirtoCommerce.InventoryModule.Data/Handlers/IndexInventoryChangedEventHandler.cs
@@ -1,26 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hangfire;
 using VirtoCommerce.InventoryModule.Core.Events;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.SearchModule.Core.Model;
-using VirtoCommerce.SearchModule.Core.Services;
+using VirtoCommerce.SearchModule.Data.BackgroundJobs;
 
 namespace VirtoCommerce.InventoryModule.Data.Handlers
 {
     public class IndexInventoryChangedEventHandler : IEventHandler<InventoryChangedEvent>
     {
-        private readonly IIndexingManager _indexingManager;
-        private static readonly EntryState[] _entityStates = new[] { EntryState.Added, EntryState.Modified, EntryState.Deleted };
-
-        public IndexInventoryChangedEventHandler(IIndexingManager indexingManager)
-        {
-            _indexingManager = indexingManager;
-        }
-
         public Task Handle(InventoryChangedEvent message)
         {
             if (message == null)
@@ -28,30 +17,13 @@ namespace VirtoCommerce.InventoryModule.Data.Handlers
                 throw new ArgumentNullException(nameof(message));
             }
 
-            var indexProductIds = message.ChangedEntries.Where(x => _entityStates.Any(s => s == x.EntryState)
-                                                                    && x.OldEntry.ProductId != null)
-                                                          .Select(x => x.OldEntry.ProductId)
-                                                          .Distinct().ToArray();
+            var indexEntries = message.ChangedEntries
+                .Select(x => new IndexEntry { Id = x.OldEntry.ProductId, EntryState = x.EntryState, Type = KnownDocumentTypes.Product })
+                .ToArray();
 
-            if (!indexProductIds.IsNullOrEmpty())
-            {
-                BackgroundJob.Enqueue(() => TryIndexInventoryBackgroundJob(indexProductIds));
-            }
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
 
             return Task.CompletedTask;
-        }
-
-
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public async Task TryIndexInventoryBackgroundJob(string[] indexProductIds)
-        {
-            await TryIndexInventory(indexProductIds);
-        }
-
-
-        protected virtual async Task TryIndexInventory(string[] indexProductIds)
-        {
-            await _indexingManager.IndexDocumentsAsync(KnownDocumentTypes.Product, indexProductIds);
         }
     }
 }
