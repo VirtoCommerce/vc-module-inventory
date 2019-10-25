f01d5c9bf05c9161e529af85ec5b7e96de227a0b
Andrey Kostyrin <kostyrin2@gmail.com>
2019-10-25 13:34:42 +0200

    #vp732 IndexInventoryChangedEventHandler (#39)
    * #vp732 IndexInventoryChangedEventHandler
    
    * #vp-732 delete index after delete entity
    
    * #vp-732 IndexingJobs
    
    * #vp-732 EntityState fix

---
+++ src/VirtoCommerce.InventoryModule.Data/Handlers/IndexInventoryChangedEventHandler.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtoCommerce.InventoryModule.Core.Events;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.SearchModule.Core.Model;
+using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+
+namespace VirtoCommerce.InventoryModule.Data.Handlers
+{
+    public class IndexInventoryChangedEventHandler : IEventHandler<InventoryChangedEvent>
+    {
+        public Task Handle(InventoryChangedEvent message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            var indexEntries = message.ChangedEntries
+                .Select(x => new IndexEntry { Id = x.OldEntry.ProductId, EntryState = EntryState.Modified, Type = KnownDocumentTypes.Product })
+                .ToArray();
+
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+
+            return Task.CompletedTask;
+        }
+    }
+}
