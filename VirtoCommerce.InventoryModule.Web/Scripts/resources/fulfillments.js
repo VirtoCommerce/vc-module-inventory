b91a4a50647a78c77bc9c7e9353ad9b6a4c4d100
Aleksandr Andreev <alivemenone@gmail.com>
2019-11-25 12:24:54 +0300

    add new endpoints

--- VirtoCommerce.InventoryModule.Web/Scripts/resources/fulfillments.js
+++ VirtoCommerce.InventoryModule.Web/Scripts/resources/fulfillments.js
@@ -3,6 +3,8 @@
         return $resource('api/inventory/fulfillmentcenters', {}, {
             get: { url: 'api/inventory/fulfillmentcenters/:id' },
             update: { method: 'PUT' },
-            search: { url: 'api/inventory/fulfillmentcenters/search', method: 'POST' }
+            search: { url: 'api/inventory/fulfillmentcenters/search', method: 'POST' },
+            getByIds: { url: 'api/inventory/fulfillmentcenters/plenty', method: 'POST', isArray: true },
+            updateBatch: { url: 'api/inventory/fulfillmentcenters/batch', method: 'POST', isArray: true }
         });
     }]);
