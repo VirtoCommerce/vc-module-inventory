7a5f479064524d27e5ff161567113f3fed2a1a7a
Aleksandr Andreev <alivemenone@gmail.com>
2019-11-25 12:18:13 +0300

    refert file

--- VirtoCommerce.InventoryModule.Web/Scripts/resources/fulfillments.js
+++ VirtoCommerce.InventoryModule.Web/Scripts/resources/fulfillments.js
@@ -2,9 +2,7 @@ angular.module('virtoCommerce.inventoryModule')
     .factory('virtoCommerce.inventoryModule.fulfillments', ['$resource', function ($resource) {
         return $resource('api/inventory/fulfillmentcenters', {}, {
             get: { url: 'api/inventory/fulfillmentcenters/:id' },
-            update: { method: 'PUT'},
-            search: { url: 'api/inventory/fulfillmentcenters/search', method: 'POST' },
-            getByIds: { url: 'api/inventory/fulfillmentcenters/plenty', method: 'POST', isArray: true },
-            updateBatch: { url: 'api/inventory/fulfillmentcenters/batch', method: 'POST', isArray: true }
+            update: { method: 'PUT' },
+            search: { url: 'api/inventory/fulfillmentcenters/search', method: 'POST' }
         });
     }]);
