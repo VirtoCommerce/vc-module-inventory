fca5c457599d74db41749baa09989897e994c24e
Andrei Kolchanov <akak1977@gmail.com>
2020-02-20 19:05:59 +0500

    2.13.9

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Inventory Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
 
-[assembly: AssemblyFileVersion("2.13.8.0")]
-[assembly: AssemblyVersion("2.13.8.0")]
+[assembly: AssemblyFileVersion("2.13.9.0")]
+[assembly: AssemblyVersion("2.13.9.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
