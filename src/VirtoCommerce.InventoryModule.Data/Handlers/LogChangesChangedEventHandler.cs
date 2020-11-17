bc3ea85ea568a488f4ea751a963188eee2312f47
Andrey A. Vasenev <avas.the.wise@gmail.com>
2020-11-17 23:06:49 +0700

    LogChangesChangedEventHandler: added the code that disables change logs for inventories if Inventory.LogInventoryChanges setting is inactive.

--- src/VirtoCommerce.InventoryModule.Data/Handlers/LogChangesChangedEventHandler.cs
+++ src/VirtoCommerce.InventoryModule.Data/Handlers/LogChangesChangedEventHandler.cs
@@ -1,38 +1,53 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Hangfire;
+using VirtoCommerce.InventoryModule.Core;
 using VirtoCommerce.InventoryModule.Core.Events;
 using VirtoCommerce.Platform.Core.ChangeLog;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.InventoryModule.Data.Handlers
 {
     public class LogChangesChangedEventHandler : IEventHandler<InventoryChangedEvent>
     {
         private readonly IChangeLogService _changeLogService;
+        private readonly ILastModifiedDateTime _lastModifiedDateTime;
+        private readonly ISettingsManager _settingsManager;
 
-        public LogChangesChangedEventHandler(IChangeLogService changeLogService)
+        public LogChangesChangedEventHandler(IChangeLogService changeLogService, ILastModifiedDateTime lastModifiedDateTime, ISettingsManager settingsManager)
         {
             _changeLogService = changeLogService;
+            _lastModifiedDateTime = lastModifiedDateTime;
+            _settingsManager = settingsManager;
         }
 
-        public virtual Task Handle(InventoryChangedEvent message)
+        public virtual async Task Handle(InventoryChangedEvent message)
         {
-            InnerHandle(message);
-            return Task.CompletedTask;
+            await InnerHandle(message);
         }
 
-        protected virtual void InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
+        protected virtual async Task InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
         {
-            var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x)).ToArray();
-            //Background task is used here for performance reasons
-            BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
+            var logInventoryChangesEnabled = await _settingsManager.GetValueAsync(ModuleConstants.Settings.General.LogInventoryChanges.Name, (bool)ModuleConstants.Settings.General.LogInventoryChanges.DefaultValue);
+
+            if (logInventoryChangesEnabled)
+            {
+                var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x)).ToArray();
+                //Background task is used here for performance reasons
+                BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
+            }
+            else
+            {
+                // Force reset the date of last data modifications, so that it would be reset even if the Inventory.LogInventoryChanges setting is inactive.
+                _lastModifiedDateTime.Reset();
+            }
         }
 
-        public void LogEntityChangesInBackground(OperationLog[] operationLogs)
+        public async Task LogEntityChangesInBackground(OperationLog[] operationLogs)
         {
-            _changeLogService.SaveChangesAsync(operationLogs).GetAwaiter().GetResult();
+            await _changeLogService.SaveChangesAsync(operationLogs);
         }
     }
 }
