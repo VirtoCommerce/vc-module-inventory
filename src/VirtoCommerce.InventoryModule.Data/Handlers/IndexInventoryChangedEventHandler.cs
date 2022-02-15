2ba142920851ae63a8b043c7e1617720cd1c8fa3
Getmansky <mge@virtoway.com>
2022-02-15 11:48:11 +0300

    Partial index update support

--- src/VirtoCommerce.InventoryModule.Data/Handlers/IndexInventoryChangedEventHandler.cs
+++ src/VirtoCommerce.InventoryModule.Data/Handlers/IndexInventoryChangedEventHandler.cs
@@ -1,22 +1,29 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.InventoryModule.Core.Events;
+using VirtoCommerce.InventoryModule.Data.Search.Indexing;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Jobs;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+using VirtoCommerce.SearchModule.Data.Services;
 
 namespace VirtoCommerce.InventoryModule.Data.Handlers
 {
     public class IndexInventoryChangedEventHandler : IEventHandler<InventoryChangedEvent>
     {
         private readonly ISettingsManager _settingsManager;
+        private readonly IEnumerable<IndexDocumentConfiguration> _configurations;
 
-        public IndexInventoryChangedEventHandler(ISettingsManager settingsManager)
+        public IndexInventoryChangedEventHandler(ISettingsManager settingsManager,
+            IEnumerable<IndexDocumentConfiguration> configurations)
         {
             _settingsManager = settingsManager;
+            _configurations = configurations;
         }
 
         public Task Handle(InventoryChangedEvent message)
@@ -29,10 +36,16 @@ namespace VirtoCommerce.InventoryModule.Data.Handlers
                 }
 
                 var indexEntries = message.ChangedEntries
-                    .Select(x => new IndexEntry { Id = x.OldEntry.ProductId, EntryState = EntryState.Modified, Type = KnownDocumentTypes.Product })
+                    .Select(x => new IndexEntry
+                    {
+                        Id = x.OldEntry.ProductId,
+                        EntryState = EntryState.Modified,
+                        Type = KnownDocumentTypes.Product
+                    })
                     .ToArray();
 
-                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries, JobPriority.Normal,
+                    _configurations.GetBuildersForProvider(typeof(ProductAvailabilityChangesProvider)).ToList());
             }
 
             return Task.CompletedTask;
