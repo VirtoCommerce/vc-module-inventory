6a95b59e67b02d9a0abd44307bb3b9fb30ba4bd8
Oleg Zhuk <zhukoo@gmail.com>
2025-12-10 12:29:25 +0200

    VCST-1752: Fix performance degradation on SQL Server with OPENJSON
    fix: Refactor DB provider setup to use platform Use<T>Database helper

---
+++ src/VirtoCommerce.InventoryModule.Data.PostgreSql/PostgreSqlDataAssemblyMarker.cs
@@ -0,0 +1,5 @@
+namespace VirtoCommerce.InventoryModule.Data.PostgreSql;
+
+public class PostgreSqlDataAssemblyMarker
+{
+}
---
+++ src/VirtoCommerce.InventoryModule.Data.SqlServer/SqlServerDataAssemblyMarker.cs
@@ -0,0 +1,5 @@
+namespace VirtoCommerce.InventoryModule.Data.SqlServer;
+
+public class SqlServerDataAssemblyMarker
+{
+}
